0f4dbba8432a079642f9ae535ff1b0925b9e1a9e
frrosaless <fr.rosaless@duocuc.cl>
2025-09-22 04:50:06 -0300

    Primera conexion a BD
    Se establece y prueba la conexion a una base de datos oracle local a traves de Docker

--- PrimerDjango/PrimerDjango/settings.py
+++ PrimerDjango/PrimerDjango/settings.py
@@ -72,14 +72,28 @@ WSGI_APPLICATION = 'PrimerDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/free', 
+        'USER': 'C##cs_user1',
+        'PASSWORD': 'chromieStoreU1',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

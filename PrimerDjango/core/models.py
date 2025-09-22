0055d2c772989d69a4c46d04045b3308c7f0952e
frrosaless <fr.rosaless@duocuc.cl>
2025-09-22 12:31:24 -0300

    Instalacion de Pillow

--- PrimerDjango/core/models.py
+++ PrimerDjango/core/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # categoria, marca, producto, proveedor, cliente, venta, detalle venta
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='categorias/', null=True, blank=True)
 
     def __str__(self):
         return self.nombre
